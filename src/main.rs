0a91d704ef5a39bb2b957d0b9c7c7a56d0ae1c2a
Andrew Gould <andrewgould935@gmail.com>
2024-07-13 22:53:19 -0400

    Added remaining functionality of thread pools and graceful shutdown procedure.

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
     let thread_pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         thread_pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down!");
 }
 
 fn handle_connection(mut stream: TcpStream) {
