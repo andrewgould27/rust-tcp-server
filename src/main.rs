db6db214d2ef8ee30aeea1a6c863825eccf6a957
Andrew Gould <andrewgould935@gmail.com>
2024-07-13 22:44:46 -0400

    Added thread pool, workers, and jobs to support multiple threads to support more than a single request at a time.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    thread
 };
+
+use rust_tcp_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    let thread_pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        thread_pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,13 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // Get request page, otherwise return 404
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" || request_line == "GET /index.html HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "./src/html/index.html")
-    } else if request_line == "GET /about.html HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "./src/html/about.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "./src/html/404.html")
+    // Map for routes to files
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/html/index.html"),
+        "GET /about HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/html/about.html"),
+        "GET /test HTTP/1.1" => ("HTTP/1.1 200 OK", "./src/html/test.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "./src/html/404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
